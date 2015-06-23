7b0887a731f646b8658de06fb9deb5df3535a8f2
Hari hara prasad Viswanathan <hr.hr.prasath@gmail.com>
2015-06-23 15:51:34 +0530

    added project files
    added project files

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Hari\Documents\GitHub\NgFileExplorer\NgFileExplorer\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Hari\Documents\GitHub\NgFileExplorer\NgFileExplorer\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Hari\Documents\GitHub\NgFileExplorer\NgFileExplorer\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Hari\Documents\GitHub\NgFileExplorer\NgFileExplorer\platforms\android\AndroidManifest.xml \
